be17b83af8061b18d0107bb69abff10e429e6ea0
refan fernando <refanfernando@gmail.com>
2019-09-11 20:50:24 +0700

    Tambah fungsi post,update,delete

--- index.js
+++ index.js
@@ -19,18 +19,30 @@ app.group("/api/v1", (router) => {
 
     //todos API Categories
     router.get('/Categories', Categories.index)    
-    router.get('/Categories/:id', Categories.show)    
+    router.get('/Categories/:id', Categories.show)
+    router.post('/Categories', Categories.store)
+    router.patch('/Categories/:id', Categories.update)    
+    router.delete('/Categories/:id', Categories.destroy)         
 
     router.get('/Menus', Menus.index)    
     router.get('/Menus/:id', Menus.show)    
+    router.post('/Menus', Menus.store)
+    router.patch('/Menus/:id', Menus.update)    
+    router.delete('/Menus/:id', Menus.destroy)
 
     router.get('/Orders', Orders.index)    
     router.get('/Orders/:id', Orders.show) 
-    router.post('/Orders', Orders.store)       
+    router.post('/Orders', Orders.store)    
+    router.post('/Orders', Orders.store)
+    router.patch('/Orders/:id', Orders.update)    
+    router.delete('/Orders/:id', Orders.destroy)   
 
     router.get('/Transactions', Transactions.index)    
     router.get('/Transactions/:id', Transactions.show)
-    router.post('/Transactions', Transactions.store)       
+    router.post('/Transactions', Transactions.store)   
+    router.post('/Transactions', Transactions.store)
+    router.patch('/Transactions/:id', Transactions.update)    
+    router.delete('/Transactions/:id', Transactions.destroy)    
 
 })
 
